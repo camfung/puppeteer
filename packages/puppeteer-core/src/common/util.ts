c0232b6a35fe77700554a551548b0cc19e0a5dea
Nikolay Vitkov <nvitkov@chromium.org>
2024-07-16 10:09:44 +0200

    feat!: change Buffer to Uint8Array

--- packages/puppeteer-core/src/common/util.ts
+++ packages/puppeteer-core/src/common/util.ts
@@ -219,8 +219,8 @@ export async function importFSPromises(): Promise<typeof FS> {
 export async function getReadableAsBuffer(
   readable: ReadableStream<Uint8Array>,
   path?: string
-): Promise<Buffer | null> {
-  const buffers: Uint8Array[] = [];
+): Promise<Uint8Array | null> {
+  const buffers: number[] = [];
   const reader = readable.getReader();
   if (path) {
     const fs = await importFSPromises();
@@ -231,7 +231,7 @@ export async function getReadableAsBuffer(
         if (done) {
           break;
         }
-        buffers.push(value);
+        buffers.push(...value);
         await fileHandle.writeFile(value);
       }
     } finally {
@@ -243,11 +243,15 @@ export async function getReadableAsBuffer(
       if (done) {
         break;
       }
-      buffers.push(value);
+      buffers.push(...value);
     }
   }
   try {
-    return Buffer.concat(buffers);
+    const concat = new Uint8Array(buffers);
+    if (concat.length === 0) {
+      return null;
+    }
+    return concat;
   } catch (error) {
     debugError(error);
     return null;
